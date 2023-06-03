0b199147907636f5675abb5a84f9c253bbeb98f6
ygz <yagiz.gnc00@gmail.com>
2023-06-03 14:59:32 +0300

    Add project files.

---
+++ TestApı/Controllers/TestClassController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApı.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestClassController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<TestClass>>> Get()
+        {
+            var testObjects = new List<TestClass> 
+            {
+                new TestClass{Id=1,FirstName="ddfshds",LastName="dshfhf"},
+                new TestClass{Id=2,FirstName="trutut",LastName="kekekekek"}            
+            };
+
+            return Ok(testObjects);
+        }
+    }
+}
---
+++ TestApı/TestClass.cs
@@ -0,0 +1,9 @@
+﻿namespace TestApı
+{
+    public class TestClass
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
